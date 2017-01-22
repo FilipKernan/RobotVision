e5b7031df5cc2d87dec04b8c313220e6086bd759
FilipKernan <filip.kernan@gmail.com>
2017-01-22 11:40:33 -0500

    turtud7tuy

--- src/eyeOfTheTiger.py
+++ src/eyeOfTheTiger.py
@@ -54,8 +54,8 @@ def capture():
         
         
         # Display the resulting frame 
-        #cv2.imshow('frame', frame)
-        cv2.imshow('mask', hsv)
+        cv2.imshow('frame', frame)
+        #cv2.imshow('mask', hsv)
         #cv2.imshow('res', res)
         #print(res.centroid.x)
         #print(res.centroid.y)
