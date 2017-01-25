4a8ccd120b59fe3666fd41a641c00f4a41c46eba
FilipKernan <filip.kernan@gmail.com>
2017-01-24 19:29:55 -0500

    draw box around the area

--- src/eyeOfTheTiger.py
+++ src/eyeOfTheTiger.py
@@ -77,6 +77,15 @@ def capture():
         print(np.array_str(calibration_box(frame)))
         cv2.imshow("NerdyCalibration", frame)
         
+        contours,hierarchy = cv2.findContours(mask, 1, 2)
+        cnt = contours[0]
+        M = cv2.moments(cnt)
+        print M
+        
+        rect = cv2.minAreaRect(cnt)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.drawContours(img,[box],0,(0,0,255),2)
         # Display the resulting frame 
         cv2.imshow('frame', frame1)
         #cv2.imshow('mask', mask)
