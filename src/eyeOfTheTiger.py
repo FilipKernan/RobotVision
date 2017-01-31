0ff6dc5b342d2baec27041292474a6c8d35be0f0
FilipKernan <filip.kernan@gmail.com>
2017-01-31 17:35:32 -0500

    sdlhfosdhofh

--- src/eyeOfTheTiger.py
+++ src/eyeOfTheTiger.py
@@ -3,18 +3,11 @@
 # and open the template in the editor.
 import numpy as np
 import cv2
-import picamera 
-#import subprocess
-import io
 import logging
 import math
+import os
 import time
 from numpy import log
-import dis
-
-
-
-
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -56,7 +49,9 @@ def calibration_box(img):
     average_color = np.uint8([[average_color]])
     
     return average_color
+
 def findDistance(area):
+    #puts the area through the function to get distace
     area = area - 1251.1
     dis = math.pow(area, -0.27052)
     dis = dis * 545.16
@@ -78,82 +73,35 @@ def findTargets(contours):
 
 
 def capture():
-    #camera.start_preview()
-    cap1 = cv2.VideoCapture(0)
-    
-
-    #camera.capture(stream, format='bgr')
+    cap = cv2.VideoCapture(0)
     time.sleep(2)
-    # adjust camera settings
-    
 
     while(True):
         # Capture frame-by-frame
-        
-        _, frame = cap1.read()
-    
-    
-        frame = cv2.GaussianBlur(frame, (11,11), 0)
-        #data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-        
-        #frame = cv2.imdecode(data, 1)
-        
-        
+        _, frame = cap.read()
         #operations on frame
-        #
-        #
-        
-
+        frame = cv2.GaussianBlur(frame, (11,11), 0)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        
-     
-        
         mask = cv2.inRange(hsv, LOWER_GREEN, UPPER_GREEN)
-        
         res = cv2.bitwise_and(frame,frame, mask = mask) 
-
-      
-        
         _, cnts, _= cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #cv2.imshow('cnts', cnts)
-        #temp use gyro for angle of attack
-        #IDK for angle of elevation
-        #area = cv2.contourArea(cnts)
-        #(area)
-        #(np.array_str(calibration_box(frame)))
-        
-#         ret,thresh = cv2.threshold(mask,50,130,200)
-#         contours,hierarchy = cv2.findContours(mask, 1, 2)
-#         cnt = contours[0]
-#         M = cv2.moments(cnt)
-#          M
-#         
-#         rect = cv2.minAreaRect(cnt)
-#         box = cv2.boxPoints(rect)
-#         box = np.int0(box)
-#         cv2.drawContours(img,[box],0,(0,0,255),2)
         try:
             if len(cnts) > 0:
-                
-                #a = max(cnts, key = cv2.contourArea)
-                
-                #area = cv2.contourArea(c)
-                
-                 
-                c,d = findTargets(cnts)
-                
-                nearStrip = polygon(c)
-                farStrip = polygon(d)    
-                # Display the resulting frame 
+                # finds the two largest areas
+                largestArea,secondLargestArea = findTargets(cnts)
+                #converts the areas into a polygon 
+                nearStrip = polygon(largestArea)
+                farStrip = polygon(secondLargestArea)    
+                # Gets the average area and then finds the distance using that  
                 area = 0
                 for i in range (0,40):
                     area = area + cv2.contourArea(nearStrip) + cv2.contourArea(farStrip)
                 area = area /41
                 dis = findDistance(area)
                 area1 = cv2.contourArea(nearStrip) + cv2.contourArea(farStrip)
-                
+                #prints the distance to the center between the two strips
                 print(dis)
+                #draw the contours on the res display 
                 cv2.drawContours(res, [nearStrip], 0, (0,0,255), 5)
                 cv2.drawContours(res, [farStrip], 0, (255,0,0), 5)
         except cv2.error:
@@ -161,23 +109,18 @@ def capture():
         cv2.imshow('frame', frame)
         #cv2.imshow('mask', mask)
         cv2.imshow('res', res)
-        #cv2.imshow('cnt', cnts)
-        #(res.centroid.x)
-        #(res.centroid.y)
-        
          # capture a keypress
         key = cv2.waitKey(20) & 0xFF
         # escape key
         if key == 27:
             break
-    #camera.release()
+    cap.release()
     cv2.destroyAllWindows()
-    #use find contures and use area
+    
 
 
 if __name__ == "__main__":
-    
     print(cv2.__version__)
-    
     capture()
 
+#TODO: write code for angle and then use networkTables
