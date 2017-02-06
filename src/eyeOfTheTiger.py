47dac9f43b899fc809567674f6a1b2ffac2b7fb1
FilipKernan <filip.kernan@gmail.com>
2017-02-06 16:11:13 -0500

    will now get the angle

--- src/eyeOfTheTiger.py
+++ src/eyeOfTheTiger.py
@@ -70,7 +70,7 @@ def findAngle(near, far, frame, dis):
            angle = math.atan(dis/dis1)
            #angle = math.degrees(angle)
            #if this doesn't work use the pythorian therom and use arcTan
-           return dis
+           return dis1
        
 def calc_center(M):
     """Detect the center given the moment of a contour."""
